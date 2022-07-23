7e92eece86bea4dd84aae610a0a334f2d60a4e5e
spiderface721 <borys2323@onet.pl>
2022-07-23 16:34:25 +0200

    input system

--- Assets/_Scripts/PigMovement.cs
+++ Assets/_Scripts/PigMovement.cs
@@ -9,34 +9,34 @@ public class PigMovement : MonoBehaviour
     [SerializeField] float movementDistanceTopBottomX;
     [SerializeField] float movementDistanceTopBottomY;
 
-    int moveDir;    //0 - left, rest clockwised
+    [SerializeField] int moveDir;    //1 - left, rest clockwise
 
     void RandomizeDir()
     {
         moveDir = Random.Range(1, 7);      
     }
-    void MakeMove()
+    void MoveToMoveDir()
     {
         Debug.Log(moveDir);
         switch (moveDir)
         {
             case 1:
-                transform.position += new Vector3(-movementDistanceSides, 0);
+                MoveInDirIfPossible(new Vector2(-movementDistanceSides, 0f));
                 break;
             case 2:
-                transform.position += new Vector3(-movementDistanceTopBottomX, movementDistanceTopBottomY);
+                MoveInDirIfPossible(new Vector2(-movementDistanceTopBottomX, movementDistanceTopBottomY));
                 break;
             case 3:
-                transform.position += new Vector3(movementDistanceTopBottomX, movementDistanceTopBottomY);
+                MoveInDirIfPossible(new Vector2(movementDistanceTopBottomX, movementDistanceTopBottomY));
                 break;
             case 4:
-                transform.position += new Vector3(movementDistanceSides, 0);
+                MoveInDirIfPossible(new Vector2(movementDistanceSides, 0));
                 break;
             case 5:
-                transform.position += new Vector3(movementDistanceTopBottomX, -movementDistanceTopBottomY);
+                MoveInDirIfPossible(new Vector2(movementDistanceTopBottomX, -movementDistanceTopBottomY));
                 break;
             case 6:
-                transform.position += new Vector3(-movementDistanceTopBottomX, -movementDistanceTopBottomY);
+                MoveInDirIfPossible(new Vector2(-movementDistanceTopBottomX, -movementDistanceTopBottomY));
                 break;
             default:
                 Debug.LogError("MOVE DIRECTION NUMBER NOT IDenTIFIED");
@@ -44,15 +44,43 @@ public class PigMovement : MonoBehaviour
         }
 
     }
+
+    bool MoveInDirIfPossible(Vector3 newVector)
+    {
+        if (!CheckForBlocks(newVector.normalized))
+        {
+            transform.position += newVector;    //MOVE
+            return true;
+        }
+        else
+        {
+            Debug.Log("HIT A WALL CANT MOVE LOL");
+            return false;
+        }
+    }
+
+    bool CheckForBlocks(Vector2 raycastRotation)
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.TransformDirection(raycastRotation), 10f);
+        if (hit)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
-        MakeMove();
+        //CheckForBlocks(Vector2.right);
+        moveDir = 6;
+        MoveToMoveDir();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 }
